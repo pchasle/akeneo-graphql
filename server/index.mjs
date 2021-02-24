6fc772d213e899145a6e257e3dcfef32b3306a57
Thomas Galvaing <thomas.galvaing@akeneo.com>
2021-02-24 18:01:57 +0100

    setup express-graphql

--- server/index.mjs
+++ server/index.mjs
@@ -1,12 +1,36 @@
-import express from "express";
+import express from 'express';
+import {graphqlHTTP} from 'express-graphql';
+import { buildSchema } from 'graphql';
 
-const app = express();
+var app = express();
 const port = 5000;
 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+/*
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+*/
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
