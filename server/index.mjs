a16249fd83dc3f462a762d97ab7c793908610403
Paul Chasle <pchasle@users.noreply.github.com>
2021-03-03 17:59:45 +0100

    add grapql query to fetch a product

--- server/index.mjs
+++ server/index.mjs
@@ -1,29 +1,75 @@
-import express from 'express';
-import {graphqlHTTP} from 'express-graphql';
-import { buildSchema } from 'graphql';
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+import fetch from "node-fetch";
+import { ResourceOwnerPassword } from "simple-oauth2";
+
+async function auth() {
+  const config = {
+    client: {
+      id: "3_5mztbe8339k4wsk8ow0wcsw8gwwoo0kss00c8gks4g4wccgow4",
+      secret: "a56yx4zji74k8gkko4o8w40wgsosk0g88g4sc8c4scw0okkks",
+    },
+    auth: {
+      tokenHost: "http://localhost:8080",
+      tokenPath: "/api/oauth/v1/token",
+    },
+  };
+
+  const client = new ResourceOwnerPassword(config);
+
+  const accessToken = await client.getToken(
+    {
+      username: "alkemics_0000",
+      password: "406b0baf3",
+    },
+    {
+      json: true,
+    }
+  );
+
+  return accessToken;
+}
 
 var app = express();
 const port = 5000;
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type User {
+    identifier: String
+  }
+
   type Query {
-    hello: String
+    product(identifier: String): User
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
+  product: async ({ identifier }) => {
+    const response = await fetch(
+      `http://localhost:8080/api/rest/v1/products/${identifier}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken.token.access_token}`,
+        },
+      }
+    );
+    const product = await response.json();
+
+    return product;
   },
 };
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 /*
 app.get("/", (req, res) => {
@@ -31,6 +77,6 @@ app.get("/", (req, res) => {
 });
 */
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
